a33c32c8b7028cfd901f38959f51eca057be1fec
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2022-11-17 20:37:57 +0200

    [ADD] Test Main

--- hw/big/survival/src/Main.java
+++ hw/big/survival/src/Main.java
@@ -1,25 +1,30 @@
-import field.Field;
-import figures.Hero;
-import figures.consumables.Broccoli;
-import figures.consumables.Mushroom;
-import figures.consumables.Rice;
+import game.Game;
+import game.figures.Hero;
+import game.figures.consumables.Beer;
+import game.figures.consumables.Broccoli;
+import game.figures.consumables.Mushroom;
+import game.figures.consumables.Orange;
+import game.figures.consumables.Pizza;
+import game.figures.consumables.Rice;
 
 public class Main {
 
   public static void main(String[] args) {
     try {
-      //Game game = new Game(2, 3);
-      // Test
+      Game game = new Game(3, 3);
+      game.addFigure(new Hero("Omori", 2000, 300));
+      game.addFigure(new Hero("Basil", 1500, 200));
+      game.addFigure(new Hero("Hero", 2500, 400));
 
-      /*       Field field = new Field(2, 3);
-      field.print();
-      field.addFigure(new Hero("Hero", 100, 10));
-      field.print();
-      field.addFigure(new Mushroom(2));
-      field.print(); */
+      game.addFigure(new Broccoli(3));
+      game.addFigure(new Rice(1));
+      game.addFigure(new Mushroom(5));
 
+      game.addFigure(new Beer(2));
+      game.addFigure(new Orange(4));
+      game.addFigure(new Pizza(5));
 
-      //game.start();
+      game.start();
     } catch (Exception e) {
       System.out.println(e.getMessage());
       System.out.println(e.getStackTrace());
